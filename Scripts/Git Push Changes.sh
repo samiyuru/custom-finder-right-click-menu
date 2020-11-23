abd5b5b7b7a59eb26e93f026cdb753fdd4c8d48f
samiyuru <samiyuru@gmail.com>
2020-11-22 21:10:38 -0500

    Adding git commands.

---
+++ Scripts/Git Push Changes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+######################################################
+# Created by Samiyuru Senarathne on 1/22/20.
+# Copyright Samiyuru Senarathne 2020
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY
+# OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+# OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+# OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Open terminal in the path given by the param.
+# Param is the directory to open the Terminal in.
+######################################################
+
+cd "$1"
+git add --all
+git commit -m "$(date)"
+git push
